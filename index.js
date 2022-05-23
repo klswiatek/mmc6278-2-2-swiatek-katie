26eb1f39011b2238ea0c20cddb03962627395351
klswiatek <klswiatek@yahoo.com>
2022-05-23 00:30:36 -0400

    birthday question added

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function greet() {
     let person = prompt("What is your name?", ""); 
     alert ("Hello," + person);
     let age = prompt ("How old are you?", "");
-    alert (parseInt(+ age))     
-    }
+    alert (parseInt(+ age));
+    confirm ("Have you had a birthday this year?");
+    }     
+   
